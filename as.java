2382e335167292c122fddfcb116a0be41821c5b2
Yathish Chandrashekar <yathish.c29@gmail.com>
2016-10-25 02:37:55 -0700

    Add files via upload

---
+++ as.java
@@ -0,0 +1,16 @@
+package org.yc.myjava.github;
+
+
+public class as {
+	/** @author YC
+	**/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+
+}
